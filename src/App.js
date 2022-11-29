488f94bec62f63667ddaeb35890b31d9748a4798
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-29 20:52:00 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Submenu />
+      <Sidebar />
+    </>
+  );
+};
+
+export default App;
---
+++ src/context.js
@@ -0,0 +1,41 @@
+import React, { useContext, useState } from "react";
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        openSubmenu,
+        closeSidebar,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
